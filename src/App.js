fd8644839eed2ffcc9a5a1011922266077b9c0b0
JeddHoy <ddej_hoy@hotmail.com>
2020-08-17 15:31:35 +0800

    sign-in form component

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import './App.css';
 import Header from './components/header/header.component'
 import HomePage from './pages/homepage/homepage.component';
 import ShopPage from './pages/shop/shop.components';
+import SignInAndSignUp from './pages/sign-in-and-sign-up/sign-in-and-sign-up.component'
 import { Switch, Route } from 'react-router-dom';
 
 
@@ -15,6 +16,7 @@ function App() {
       <Switch>
       <Route exact path='/' component={HomePage}/> 
       <Route path='/shop' component={ShopPage} />
+      <Route path='/signin' component={SignInAndSignUp} />
       </Switch>   
     </div>
   );
