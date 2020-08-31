c0a072448fb4e35fe8be456a7b7018b0c7c65fe2
JeddHoy <ddej_hoy@hotmail.com>
2020-08-31 17:02:53 +0800

    directory and collection added

---
+++ src/pages/collection/collection.component.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import CollectionItem from '../../components/collection-item/collection-item.component'
+
+
+const CollectionPage = () => (
+    <div className='collection-page'>
+        <h2>COLLECTION PAGE</h2>
+    </div>
+)
+
+export default CollectionPage;
--- src/pages/shop/shop.components.jsx
+++ src/pages/shop/shop.components.jsx
@@ -1,32 +1,18 @@
 import React from 'react';
-import SHOP_DATA from './shop.data';
-import CollectionPreview from '../../components/collection-preview/collection-preview';
+import {Route} from 'react-router-dom'
 
-class ShopPage extends React.Component {
-    constructor(props){
-        super(props);
-        
-        this.state = {
-            collections: SHOP_DATA
-        }
-    }
+import CollectionsOverview from '../../components/collections-overview/collections-overview.component'
+import CollectionPage from '../collection/collection.component'
 
-    render() {
-        
-        const {collections} = this.state;
 
-        return(            
+const ShopPage = ({match}) => (
+    <div className='shop-page'>
+        <Route exact path={`${match.path}`} component={CollectionsOverview}/>
+        <Route path={`${match.path}/:collectionId`} component={CollectionPage}/>
+    </div>
+)
 
-            <div className='shop-page'>
-                {
-                    collections.map(({id, ...otherCollectionsProps}) => (
-                        <CollectionPreview key={id} {...otherCollectionsProps}/>
-                    ))
-                }
-            </div>
-        )
-    }
-    
-}
+
+
 
 export default ShopPage;
